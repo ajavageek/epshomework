e7026e0568153fc038d88a1836a2dc8930c05512
Nicolas Frankel <nicolas@frankel.ch>
2019-05-19 11:24:20 +0200

    Migrate strongly-typed function references to string-based reflection

--- src/main/kotlin/ch/frankel/blog/eps/17-Reflective.kt
+++ src/main/kotlin/ch/frankel/blog/eps/17-Reflective.kt
@@ -1,23 +1,33 @@
 package ch.frankel.blog.eps
 
-fun stopWords() = read("stop_words.txt")
-    .flatMap { it.split(",") }
+import kotlin.reflect.full.declaredMemberFunctions
+import kotlin.reflect.full.primaryConstructor
 
-fun extractWords(filename: String) = read(filename)
-    .flatMap { it.toLowerCase().split("\\W|_".toRegex()) }
-    .filter { it.isNotBlank() && it.length >= 2 }
+class Reflective {
 
-fun frequencies(words: List<String>) = words.groupBy { it }
-    .map { it.key to it.value.size }
-    .sortedBy { it.second }
-    .takeLast(25)
-    .toMap()
+    fun stopWords() = read("stop_words.txt")
+        .flatMap { it.split(",") }
+
+    fun extractWords(filename: String) = read(filename)
+        .flatMap { it.toLowerCase().split("\\W|_".toRegex()) }
+        .filter { it.isNotBlank() && it.length >= 2 }
+
+    fun frequencies(words: List<String>) = words.groupBy { it }
+        .map { it.key to it.value.size }
+        .sortedBy { it.second }
+        .takeLast(25)
+        .toMap()
+}
 
 fun run(filename: String): Map<String, Int> {
-    val funcStopWords = ::stopWords
-    val funcExtractWords = ::extractWords
-    val funcFrequencies = ::frequencies
-    val words = funcExtractWords.invoke(filename) - funcStopWords.invoke()
-    return funcFrequencies.invoke(words)
+    val clazz = Class.forName("ch.frankel.blog.eps.Reflective").kotlin
+    val funcStopWords = clazz.declaredMemberFunctions.single { it.name == "stopWords" }
+    val funcExtractWords = clazz.declaredMemberFunctions.single { it.name == "extractWords" }
+    val funcFrequencies = clazz.declaredMemberFunctions.single { it.name == "frequencies" }
+    val constructor = clazz.primaryConstructor
+    val instance = constructor?.call()
+    val words = funcExtractWords.call(instance, filename) as List<String>
+    val stopWords = funcStopWords.call(instance) as List<String>
+    return funcFrequencies.call(instance, words - stopWords) as Map<String, Int>
 }
 
