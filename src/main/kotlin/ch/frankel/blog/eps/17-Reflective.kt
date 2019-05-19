d1ba340be3229cca9a975aa85423f05c80440766
Nicolas Frankel <nicolas@frankel.ch>
2019-05-19 11:25:38 +0200

    Migrate Kotlin's reflection API to Java's reflection API

--- src/main/kotlin/ch/frankel/blog/eps/17-Reflective.kt
+++ src/main/kotlin/ch/frankel/blog/eps/17-Reflective.kt
@@ -1,33 +1,24 @@
 package ch.frankel.blog.eps
 
-import kotlin.reflect.full.declaredMemberFunctions
-import kotlin.reflect.full.primaryConstructor
+fun stopWords() = read("stop_words.txt")
+    .flatMap { it.split(",") }
 
-class Reflective {
+fun extractWords(filename: String) = read(filename)
+    .flatMap { it.toLowerCase().split("\\W|_".toRegex()) }
+    .filter { it.isNotBlank() && it.length >= 2 }
 
-    fun stopWords() = read("stop_words.txt")
-        .flatMap { it.split(",") }
-
-    fun extractWords(filename: String) = read(filename)
-        .flatMap { it.toLowerCase().split("\\W|_".toRegex()) }
-        .filter { it.isNotBlank() && it.length >= 2 }
-
-    fun frequencies(words: List<String>) = words.groupBy { it }
-        .map { it.key to it.value.size }
-        .sortedBy { it.second }
-        .takeLast(25)
-        .toMap()
-}
+fun frequencies(words: List<String>) = words.groupBy { it }
+    .map { it.key to it.value.size }
+    .sortedBy { it.second }
+    .takeLast(25)
+    .toMap()
 
 fun run(filename: String): Map<String, Int> {
-    val clazz = Class.forName("ch.frankel.blog.eps.Reflective").kotlin
-    val funcStopWords = clazz.declaredMemberFunctions.single { it.name == "stopWords" }
-    val funcExtractWords = clazz.declaredMemberFunctions.single { it.name == "extractWords" }
-    val funcFrequencies = clazz.declaredMemberFunctions.single { it.name == "frequencies" }
-    val constructor = clazz.primaryConstructor
-    val instance = constructor?.call()
-    val words = funcExtractWords.call(instance, filename) as List<String>
-    val stopWords = funcStopWords.call(instance) as List<String>
-    return funcFrequencies.call(instance, words - stopWords) as Map<String, Int>
+    val clazz = Class.forName("ch.frankel.blog.eps._17_ReflectiveKt")
+    val funcStopWords = clazz.getMethod("stopWords")
+    val funcExtractWords = clazz.getMethod("extractWords", String::class.java)
+    val funcFrequencies = clazz.getMethod("frequencies", List::class.java)
+    val words = funcExtractWords.invoke(null, filename) as List<String>
+    val stopWords = funcStopWords.invoke(null) as List<String>
+    return funcFrequencies.invoke(null, words - stopWords) as Map<String, Int>
 }
-
