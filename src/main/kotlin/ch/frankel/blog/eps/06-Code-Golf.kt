251c812b46727c22809bb51d16a6afacac3ca1e6
Nicolas Frankel <nicolas@frankel.ch>
2019-05-20 18:59:08 +0200

    Add another alternative implementation

--- src/main/kotlin/ch/frankel/blog/eps/06-Code-Golf.kt
+++ src/main/kotlin/ch/frankel/blog/eps/06-Code-Golf.kt
@@ -1,9 +1,10 @@
 package ch.frankel.blog.eps
 
-fun run(filename: String) = (read(filename)
+val stopWords = read("stop_words.txt").flatMap { it.split(",") }
+
+fun run(filename: String)= read(filename)
     .flatMap { it.toLowerCase().split("\\W|_".toRegex()) }
-    .filter { it.isNotBlank() && it.length >= 2 }
-        - read("stop_words.txt").flatMap { it.split(",") })
+    .filter { it.isNotBlank() && it.length >= 2 && !stopWords.contains(it) }
     .fold(mutableMapOf<String, Int>()) { map, word ->
         map.merge(word, 1) { existing, new -> existing + new }
         map
