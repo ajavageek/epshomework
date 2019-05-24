7c3f6d9f2a6d32c805a8d579b203863ab808cd9c
Nicolas Frankel <nicolas@frankel.ch>
2019-05-24 22:57:14 +0200

    Improve compile-time checking

--- src/main/kotlin/ch/frankel/blog/eps/08-Kick-Forward.kt
+++ src/main/kotlin/ch/frankel/blog/eps/08-Kick-Forward.kt
@@ -1,50 +1,156 @@
 package ch.frankel.blog.eps
 
-import kotlin.reflect.KCallable
+import kotlin.reflect.KFunction1
+import kotlin.reflect.KFunction2
 
 fun run(filename: String): Map<String, Int> {
-    val function: KCallable<Map<String, Int>> = ::readFile
-    return function.call(filename, ::filterChars)
+    val function: KFunction2<
+            String,
+            KFunction2<
+                    List<String>,
+                    KFunction2<
+                            List<String>,
+                            KFunction2<
+                                    List<String>,
+                                    KFunction2<
+                                            List<String>,
+                                            KFunction2<
+                                                    List<String>,
+                                                    KFunction2<
+                                                            List<Pair<String, Int>>,
+                                                            KFunction1<List<Pair<String, Int>>, Map<String, Int>>,
+                                                            Map<String, Int>>,
+                                                    Map<String, Int>>,
+                                            Map<String, Int>>,
+                                    Map<String, Int>>,
+                            Map<String, Int>>,
+                    Map<String, Int>>,
+            Map<String, Int>> = ::readFile
+    return function.invoke(filename, ::filterChars)
 }
 
-fun readFile(filename: String, function: KCallable<Map<String, Int>>): Map<String, Int> {
+fun readFile(
+    filename: String,
+    function: KFunction2<
+            List<String>,
+            KFunction2<
+                    List<String>,
+                    KFunction2<
+                            List<String>,
+                            KFunction2<
+                                    List<String>,
+                                    KFunction2<
+                                            List<String>,
+                                            KFunction2<
+                                                    List<Pair<String, Int>>,
+                                                    KFunction1<List<Pair<String, Int>>, Map<String, Int>>,
+                                                    Map<String, Int>>,
+                                            Map<String, Int>>,
+                                    Map<String, Int>>,
+                            Map<String, Int>>,
+                    Map<String, Int>>,
+            Map<String, Int>>
+): Map<String, Int> {
     val data = read(filename)
-    return function.call(data, ::normalize)
+    return function.invoke(data, ::normalize)
 }
 
-fun filterChars(lines: List<String>, function: KCallable<Map<String, Int>>): Map<String, Int> {
+fun filterChars(
+    lines: List<String>,
+    function: KFunction2<
+            List<String>,
+            KFunction2<
+                    List<String>,
+                    KFunction2<
+                            List<String>,
+                            KFunction2<
+                                    List<String>,
+                                    KFunction2<
+                                            List<Pair<String, Int>>,
+                                            KFunction1<List<Pair<String, Int>>, Map<String, Int>>,
+                                            Map<String, Int>>,
+                                    Map<String, Int>>,
+                            Map<String, Int>>,
+                    Map<String, Int>>,
+            Map<String, Int>>
+): Map<String, Int> {
     val pattern = "\\W|_".toRegex()
     val filtered = lines
         .map { it.replace(pattern, " ") }
-    return function.call(filtered, ::scan)
+    return function.invoke(filtered, ::scan)
 }
 
-fun normalize(lines: List<String>, function: KCallable<Map<String, Int>>): Map<String, Int> {
+fun normalize(
+    lines: List<String>,
+    function: KFunction2<
+            List<String>,
+            KFunction2<
+                    List<String>,
+                    KFunction2<
+                            List<String>,
+                            KFunction2<
+                                    List<Pair<String, Int>>,
+                                    KFunction1<List<Pair<String, Int>>, Map<String, Int>>,
+                                    Map<String, Int>>,
+                            Map<String, Int>>,
+                    Map<String, Int>>,
+            Map<String, Int>>
+): Map<String, Int> {
     val normalized = lines.map { it.toLowerCase() }
-    return function.call(normalized, ::removeStopWords)
+    return function.invoke(normalized, ::removeStopWords)
 }
 
-fun scan(lines: List<String>, function: KCallable<Map<String, Int>>): Map<String, Int> {
+fun scan(
+    lines: List<String>,
+    function: KFunction2<
+            List<String>,
+            KFunction2<
+                    List<String>,
+                    KFunction2<
+                            List<Pair<String, Int>>,
+                            KFunction1<List<Pair<String, Int>>, Map<String, Int>>,
+                            Map<String, Int>>,
+                    Map<String, Int>>,
+            Map<String, Int>>
+): Map<String, Int> {
     val split = lines.flatMap { it.split(" ") }
         .filter { it.isNotBlank() && it.length >= 2 }
-    return function.call(split, ::frequencies)
+    return function.invoke(split, ::frequencies)
 }
 
-fun removeStopWords(words: List<String>, function: KCallable<Map<String, Int>>): Map<String, Int> {
+fun removeStopWords(
+    words: List<String>,
+    function: KFunction2<
+            List<String>,
+            KFunction2<
+                    List<Pair<String, Int>>,
+                    KFunction1<List<Pair<String, Int>>, Map<String, Int>>,
+                    Map<String, Int>>,
+            Map<String, Int>>
+): Map<String, Int> {
     val stopWords = read("stop_words.txt").flatMap { it.split(",") }
-    return function.call(words - stopWords, ::sort)
+    return function.invoke(words - stopWords, ::sort)
 }
 
-fun frequencies(words: List<String>, function: KCallable<Map<String, Int>>): Map<String, Int> {
+fun frequencies(
+    words: List<String>,
+    function: KFunction2<
+            List<Pair<String, Int>>,
+            KFunction1<List<Pair<String, Int>>, Map<String, Int>>,
+            Map<String, Int>>
+): Map<String, Int> {
     val frequencies = words
         .groupBy { it }
         .map { it.key to it.value.size }
-    return function.call(frequencies, ::updateState)
+    return function.invoke(frequencies, ::updateState)
 }
 
-fun sort(frequencies: List<Pair<String, Int>>, function: KCallable<Map<String, Int>>): Map<String, Int> {
+fun sort(
+    frequencies: List<Pair<String, Int>>,
+    function: KFunction1<List<Pair<String, Int>>, Map<String, Int>>
+): Map<String, Int> {
     val top = frequencies.sortedBy { it.second }.takeLast(25)
-    return function.call(top)
+    return function.invoke(top)
 }
 
 fun updateState(top: List<Pair<String, Int>>) = top.toMap()
