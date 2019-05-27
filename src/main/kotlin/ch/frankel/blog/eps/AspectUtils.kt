8bfa15cd6e5487016b11933719a5480167023e1e
Nicolas Frankel <nicolas@frankel.ch>
2019-05-27 09:23:44 +0200

    Add tracing aspect

--- src/main/kotlin/ch/frankel/blog/eps/AspectUtils.kt
+++ src/main/kotlin/ch/frankel/blog/eps/AspectUtils.kt
@@ -1,8 +1,11 @@
 package ch.frankel.blog.eps
 
+import org.aspectj.lang.JoinPoint
 import org.aspectj.lang.ProceedingJoinPoint
+import org.aspectj.lang.annotation.After
 import org.aspectj.lang.annotation.Around
 import org.aspectj.lang.annotation.Aspect
+import org.aspectj.lang.annotation.Before
 
 @Aspect
 class AspectUtils {
@@ -13,7 +16,21 @@ class AspectUtils {
     fun profile(joinPoint: ProceedingJoinPoint): Any? {
         val start = System.currentTimeMillis()
         val result = joinPoint.proceed()
-        println("Call to ${joinPoint.signature} took ${System.currentTimeMillis() - start} ms")
+        println("  Call took ${System.currentTimeMillis() - start} ms")
         return result
+    }
+
+    @Before("""call (* _18_AspectsKt.extractWords(..))
+                  || call (* _18_AspectsKt.frequencies(..))
+                  || call (* _18_AspectsKt.sort(..))""")
+    fun traceBefore(joinPoint: JoinPoint) {
+        println("Start ${joinPoint.signature}")
+    }
+
+    @After("""call (* _18_AspectsKt.extractWords(..))
+                  || call (* _18_AspectsKt.frequencies(..))
+                  || call (* _18_AspectsKt.sort(..))""")
+    fun traceAfter(joinPoint: JoinPoint) {
+        println("End ${joinPoint.signature}")
     }
 }
