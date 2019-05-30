76780923da0d8bf186ff24852ff4c0004650f5a0
Nicolas Frankel <nicolas@frankel.ch>
2019-05-30 22:04:16 +0200

    Remove all state

--- src/main/kotlin/ch/frankel/blog/eps/26-Spreadsheet.kt
+++ src/main/kotlin/ch/frankel/blog/eps/26-Spreadsheet.kt
@@ -1,43 +1,48 @@
 package ch.frankel.blog.eps
 
 fun run(filename: String): Map<String, Int> {
-    val allWords: Cell<List<Any>, () -> List<Any>> = Cell(listOf<String>()) {
+    val allWords: Pair<List<Any>, () -> List<Any>> = Pair(listOf<String>()) {
         read(filename)
             .flatMap { it.toLowerCase().split("\\W|_".toRegex()) }
             .filter { it.isNotBlank() && it.length >= 2 }
     }
-    val stopWords: Cell<List<Any>, () -> List<Any>> = Cell(listOf<String>()) {
-        read("stop_words.txt")
+    val nonStopWords: Pair<List<Any>, (List<String>) -> List<Any>> = Pair(listOf()) { words: List<String> ->
+        val stopWords = read("stop_words.txt")
             .flatMap { it.split(",") }
+        words.filter { !stopWords.contains(it) }
     }
-    val nonStopWords: Cell<List<Any>, (List<String>) -> List<Any>> = Cell(listOf()) { words: List<String> ->
-        words.filter { !stopWords.value.contains(it) }
-    }
-    val counts: Cell<List<Any>, (List<String>) -> List<Pair<String, Int>>> = Cell(listOf()) { words ->
+    val counts: Pair<List<Any>, (List<String>) -> List<Pair<String, Int>>> = Pair(listOf()) { words ->
         words.groupingBy { it }.eachCount().toList()
     }
-    val sortedData: Cell<List<Any>, (List<Pair<String, Int>>) -> List<Any>> = Cell(listOf()) { frequencies ->
+    val sortedData: Pair<List<Any>, (List<Pair<String, Int>>) -> List<Any>> = Pair(listOf()) { frequencies ->
         frequencies.sortedByDescending { it.second }
     }
 
     val spreadsheet: Spreadsheet =
-        listOf(stopWords, allWords, nonStopWords, counts, sortedData)
+        listOf(allWords, nonStopWords, counts, sortedData)
 
-    update(spreadsheet)
-
-    return (sortedData.value.take(25) as List<Pair<String, Int>>).toMap()
+    return (update(spreadsheet)
+        .last()
+        .first
+        .take(25)  as List<Pair<String, Int>>)
+        .toMap()
 }
 
-private fun update(cells: Spreadsheet) {
-    cells.forEachIndexed { i, cell ->
-        val f = cell.formula
-        if (f is () -> List<Any>) cell.value = f()
+private fun update(cells: Spreadsheet): Spreadsheet {
+    tailrec fun recurseUpdate(todo: Spreadsheet, acc: Spreadsheet): Spreadsheet {
+        return if (todo.isEmpty()) acc
         else {
-            val g = f as (List<Any>) -> List<Any>
-            cell.value = g(cells[i - 1].value)
+            val column = todo.first()
+            val f = column.second
+            if (f is () -> List<Any>)
+                recurseUpdate(todo.takeLast(todo.size - 1), acc + (f() to f))
+            else {
+                val g = f as (List<Any>) -> List<Any>
+                recurseUpdate(todo.takeLast(todo.size - 1), acc + (g(acc.last().first) to f))
+            }
         }
     }
+    return recurseUpdate(cells, arrayListOf())
 }
 
-typealias Spreadsheet = List<Cell<List<Any>, out Function<List<Any>>>>
-data class Cell<T, V>(var value: T, var formula: V)
+typealias Spreadsheet = List<Pair<List<Any>, Function<List<Any>>>>
