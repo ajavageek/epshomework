1fb6727c6aa60f60138a4cf28000134a4e4fe6da
Nicolas Frankel <nicolas@frankel.ch>
2019-08-31 16:10:23 +0200

    Use Hazelcast IMDG

--- src/main/kotlin/ch/frankel/blog/eps/29-Dataspaces.kt
+++ src/main/kotlin/ch/frankel/blog/eps/29-Dataspaces.kt
@@ -1,14 +1,27 @@
 package ch.frankel.blog.eps
 
-import java.util.concurrent.ConcurrentMap
+import com.hazelcast.core.HazelcastInstance
+import com.hazelcast.core.HazelcastInstanceAware
+import java.io.Serializable
+import java.util.concurrent.Callable
 
-fun processWords(words: List<String>, frequencies: ConcurrentMap<String, Int>) {
-    val stopWords = read("stop_words.txt")
-        .flatMap { it.split(",") }
-    words.forEach {
-        if (!stopWords.contains(it))
-            frequencies.merge(it, 1) { count, value ->
-                count + value
-            }
+class ProcessWords(val words: List<String>) : Callable<Unit>, Serializable, HazelcastInstanceAware {
+
+    private lateinit var hazelcastInstance: HazelcastInstance
+
+    override fun setHazelcastInstance(hazelcastInstance: HazelcastInstance) {
+        this.hazelcastInstance = hazelcastInstance
+    }
+
+    override fun call() {
+        val frequencies = hazelcastInstance.getMap<String, Int>("map")
+        val stopWords = read("stop_words.txt")
+            .flatMap { it.split(",") }
+        words.forEach {
+            if (!stopWords.contains(it))
+                frequencies.merge(it, 1) { count, value ->
+                    count + value
+                }
+        }
     }
 }
--- src/main/kotlin/ch/frankel/blog/eps/Placeholder.kt
+++ src/main/kotlin/ch/frankel/blog/eps/Placeholder.kt
@@ -1,23 +1,24 @@
 package ch.frankel.blog.eps
 
-import java.util.concurrent.ConcurrentHashMap
-import java.util.concurrent.Executors
+import com.hazelcast.core.Hazelcast
+import com.hazelcast.core.IExecutorService
 
 /* For compiling purpose. */
 fun run(filename: String): Map<String, Int> {
-    val freqSpace = ConcurrentHashMap<String, Int>()
+    val hazelcastInstance = Hazelcast.newHazelcastInstance()
+    val freqSpace = hazelcastInstance.getMap<String, Int>("map")
+    val executorService: IExecutorService = hazelcastInstance.getExecutorService("executorService")
     val words = read(filename)
         .flatMap { it.toLowerCase().split("\\W|_".toRegex()) }
         .filter { it.isNotBlank() && it.length >= 2 }
-    val count = 4
-    val callables = words.chunked(words.size / count)
-        .map { Runnable { processWords(it, freqSpace) }}
-        .map { Executors.callable(it) }
-    val executorService = Executors.newFixedThreadPool(count)
+    val callables = words.chunked(words.size / 4)
+        .map { ProcessWords(it) }
     executorService.invokeAll(callables)
-    return freqSpace
+    val result = freqSpace
         .toList()
         .sortedByDescending { it.second }
         .take(25)
         .toMap()
+    hazelcastInstance.shutdown()
+    return result
 }
